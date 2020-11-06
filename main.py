0c108ff75fd62a810bf8765fc66d268e3085a98b
Esther <e.g.zigterman.rustenburg@st.hanze.nl>
2020-11-06 18:27:06 +0100

    comments van eisen

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 # dit is main
+
+
+# haal informatie van de schermen
+
+# bewaar data geschiedenis
+
+# toon gewenste informatie op scherm
+
+
+
+
+# dict of rolluiken / schermen + status
+
+# buttons
+
+# dictionary van sensortype + data
+    # lichtsensor per x seconden
+        # dict per uur
+    # temperatuur per x seconden
+        # dict per uur met lijst van elke temp
+    # lichtintensiteit per 30 sec
+
+# status
+
+# instellen hoe ver hij in- en uitrolt
+# instellen wanneer hij automatisch in- en uitrolt
+# toon alleen info voor verbonden apparaten
+
+# zelf uitrollen
