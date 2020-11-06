cbff148f6514a74539d4b3144ff4bc77cb7e5d23
Esther <e.g.zigterman.rustenburg@st.hanze.nl>
2020-11-06 19:40:17 +0100

    data lezen
    onderaan mogelijk idee voor lijngrafiek plotten

--- connections.py
+++ connections.py
@@ -26,3 +26,34 @@ def check_presence(correct_port, interval=0.1):
 port_controller = threading.Thread(target=check_presence, args=(arduino_port, 0.1,))
 port_controller.setDaemon(True)
 port_controller.start()
+
+
+
+# vraag om de data van de arduino. Idee is 1 regel per keer
+def read_input(arduino, interval):
+    
+    # https://problemsolvingwithpython.com/11-Python-and-External-Hardware/11.04-Reading-a-Sensor-with-Python/
+
+    # nodig om data op te halen, de 9600 is de baut. Verander dit naar de werkelijke baut die gebruikt wordt wanneer dit niet overeen komt met de code
+    arduino_serial = serial.Serial(arduino_port[0][0], 9600)
+
+    b = arduino_serial.readline()           # read a byte string
+    string_n = b.decode()                   # decode byte string into Unicode  
+    string = string_n.rstrip()              # remove \n and \r
+    flt = float(string)                     # convert string to float
+
+    arduino_serial.close()
+    return (flt)
+
+
+
+# voor het plotten van een lijngrafiek
+#import matplotlib.pyplot as plt
+# if using a Jupyter notebook include
+#%matplotlib inline
+
+#plt.plot(data)
+#plt.xlabel('Time (seconds)')
+#plt.ylabel('Potentiometer Reading')
+#plt.title('Potentiometer Reading vs. Time')
+#plt.show()
