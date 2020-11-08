b9aafb852a056b14820ecbda3bd257649656e656
Esther <e.g.zigterman.rustenburg@st.hanze.nl>
2020-11-08 10:36:35 +0100

    duidelijker gemaakt

--- data_transfer.py
+++ data_transfer.py
@@ -6,31 +6,40 @@ import time
 arduino_port = connections.arduino_port
 # vraag om de data van de arduino. Idee is 1 regel per keer. 
 # Dit moet elke 60 seconden voor alle sensors worden uitgevoerd
-def get_data(arduino):
+def get_data(port):
     
     # https://problemsolvingwithpython.com/11-Python-and-External-Hardware/11.04-Reading-a-Sensor-with-Python/
 
-    # nodig om data op te halen, de 9600 is de baut. Verander dit naar de werkelijke baut die gebruikt wordt wanneer dit niet overeen komt met de code
-    arduino_serial = serial.Serial(arduino_port[0][0], 9600)
+    # nodig om data op te halen, de 9600 is de baut. 
+    # Verander dit naar de werkelijke baut die gebruikt wordt wanneer dit niet overeen komt met de code
+    # 
+    arduino_serial = serial.Serial(port, 9600)
 
     b = arduino_serial.readline()           # read a byte string
     string_n = b.decode()                   # decode byte string into Unicode  
     string = string_n.rstrip()              # remove \n and \r
     flt = float(string)                     # convert string to float
 
-    arduino_serial.close()
+    # arduino_serial.close()
     return (flt)
 
-# methode voor het verkrijgen van de status van de rolluik / scherm (of hij ingerold is, uitgerold, of gedeeltelijk)
-def get_position(arduino):
-    # check which lamp is on on the arduino
 
-    return "onbekend"
+# methode voor het verkrijgen van de status van de rolluik / scherm (of hij ingerold is, uitgerold, of gedeeltelijk)
+def get_position(port):
+    # check which lamp is on on the arduino
+    pass
 
 # methode om de positie van de rolluik te veranderen
-def change_position(arduino, positie):
-    # tell arduino to turn on other light
-    return "onbekend"
+def set_position(port, positie):
+    # tell arduino to turn on other light and thus 'change' the position or status of the arduino
+    pass
+
+def set_max(arduino, max):
+    pass
+
+def set_min(arduino, min):
+    pass
+
 
 arduino_dict = {}
 for port in arduino_port:
--- settings.py
+++ settings.py
@@ -4,9 +4,17 @@ import data_transfer
 
 class Arduino():
 
-    def __init__(self, naam, status="onbekend"):
-        self.naam = naam
+    # arduino is 1 item uit de lijst arduino_port uit connections
+    def __init__(self, arduino, status="onbekend"):
+        self.port = arduino[0]
+        self.naam = arduino[1].split(" (COM")[0]
         self.status = status
+    
+    def set_status(self, position):
+        data_transfer.set_position(self.port, position)
+    
+    def set_naam(self, naam):
+        self.naam = naam
 
 
 
@@ -14,28 +22,49 @@ class Arduino():
 
 class Lichtsensor(Arduino):
     #bij max gaan ze naar beneden, bij min gaan ze omhoog
-    def __init__(self, naam, status="onbekend", max=80000, min=10000):
-        super().__init__(naam, status)
+    def __init__(self, arduino, status="onbekend", max=80000, min=10000):
+        super().__init__(arduino, status)
         self.min = min
         self.max = max
         self.data = str()
 
+    def change_max(self, value):
+        data_transfer.set_max(self.port, value)
+    
+    def change_min(self, value):
+        data_transfer.set_min(self.port, value)
+
 #####################################################################################################################################
 
 class Temperatuur(Arduino):
-    #bij max gaan ze naar beneden, bij min gaan ze omhoog
-    def __init__(self, naam, status="onbekend", max=22, min=18):
-        super().__init__(naam, status)
+    # Bij max gaan ze naar beneden, bij min gaan ze omhoog
+    def __init__(self, arduino, status="onbekend", max=22, min=18):
+        super().__init__(arduino, status)
         self.min = min
         self.max = max
         self.data = []
+    
+    def change_max(self, value):
+        data_transfer.set_max(self.port, value)
+    
+    def change_min(self, value):
+        data_transfer.set_min(self.port, value)
 
 #####################################################################################################################################
 
 class Luchtvochtigheid(Arduino):
     #bij max gaan ze naar beneden, bij min gaan ze omhoog
-    def __init__(self, naam, status="onbekend", max=80000, min=10000):
-        super().__init__(naam, status)
+    def __init__(self, arduino, status="onbekend", max=80000, min=10000):
+        super().__init__(arduino, status)
         self.min = min
         self.max = max
         self.data = str()
+
+    def change_max(self, value):
+        data_transfer.set_max(self.port, value)
+    
+    def change_min(self, value):
+        data_transfer.set_min(self.port, value)
+
+#####################################################################################################################################
+
