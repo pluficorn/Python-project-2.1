2e8136bbdf32102c019912716ea4e867570dfd5b
Esther <e.g.zigterman.rustenburg@st.hanze.nl>
2020-11-09 19:15:33 +0100

    Arduino.py aangepast
    klassen in arduino aangepast op meerdere sensoren per arduino i.p.v. 1 sensor per arduino

--- arduino.py
+++ arduino.py
@@ -10,13 +10,153 @@ from datetime import timedelta
 
 str = string
 
+#####################################################################################################################################
+
+class Sensor():
+
+    # Bij max gaan ze naar beneden, bij min gaan ze omhoog
+    def __init__(self, max=999999, min=0):
+        self.min = min
+        self.max = max
+        self.data = {}
+    
+    # verander het bovenste limiet
+    def change_max(self, value, port):
+        if value != self.max and value > self.min:
+            data_transfer.change_limiet(port, 'max', value)
+            self.max = value
+    
+    # verander het onderste limiet
+    def change_min(self, value, port):
+        if value != self.min and value < self.max:
+            data_transfer.change_limiet(port, 'min', value)
+            self.min = value
+
+    # verzamel de data van de arduino gebasseerd op datum
+    def collect_data(self, port):
+        datum = datetime.datetime.now()
+        jaar = datum.year
+        maand = datum.month
+        dag = datum.month
+
+        data = data_transfer.get_data(port)
+
+        if jaar in self.data:
+            if maand in self.data[jaar]:
+                if dag in self.data[jaar][maand]:
+                    self.data[jaar][maand][dag].append(data)
+                else:
+                    self.data[jaar][maand][dag] = [data]
+            else:
+                self.data[jaar][maand][dag] = [data]
+        else:
+            self.data[jaar][maand][dag] = [data]
+
+    def return_data(self, begindatum, einddatum):
+
+        b_datum = datetime.datetime(*begindatum)
+        e_datum = datetime.datetime(*einddatum)
+
+        verschil = e_datum - b_datum
+
+        if verschil <= 0:
+            pass
+
+            # gemiddelde data per uur over 1 dag
+        if verschil.days is 1:
+
+            # bereken per uur het gemiddelde en voeg het toe aan een lijst
+            l = []
+            datum = self.data[b_datum.year][b_datum.month][b_datum.day]
+            for uur in datum:
+                tijdstip =datum[uur]
+                l.append(sum(tijdstip)/len(tijdstip))
+
+        elif verschil.days is 7:
+            l = []
+            # gem per dag over 1 week
+            for i in range(verschil.days + 1):
+                datum = b_datum + timedelta(days=i)
+                dagdata = self.data[datum.year][datum.month][datum.day]
+                totaal = 0
+                lengte = 0
+
+                # tel alles van de dag op
+                for uur in dagdata:
+                    totaal = sum(dagdata[uur])
+                    lengte += len(dagdata[uur])
+
+                # voeg het gemiddelde per dag toe aan de lijst
+                l.append(totaal/lengte)
+
+        elif b_datum.month is (e_datum.month - 1) and b_datum.day is e_datum.day:
+            l = []
+            # gem per dag over 1 week
+            for i in range(verschil.days + 1):
+                datum = b_datum + timedelta(days=i)
+                dagdata = self.data[datum.year][datum.month][datum.day]
+                totaal = 0
+                lengte = 0
+
+                # tel alles van de dag op
+                for uur in dagdata:
+                    totaal += sum(dagdata[uur])
+                    lengte += len(dagdata[uur])
+
+                # voeg het gemiddelde per dag toe aan de lijst
+                l.append(totaal/lengte)
+        # over een volledig jaar of periodes gebasseerd op gemiddelde per maand
+        elif verschil is 365 or b_datum.day is e_datum.day and b_datum.month is not e_datum.month - 1:
+            for i in range(verschil.days + 1):
+                datum = b_datum + timedelta(days=i)
+                maanddata = self.data[datum.year][datum.month]
+                totaal = 0
+                lengte = 0
+                
+                # tel alles van de maand op
+                for dag in maanddata:
+                    dagdata = maanddata[dag]
+                    for uur in dagdata:
+                        totaal += sum(dagdata[uur])
+                        lengte += len(dagdata[uur])
+                
+                l.append(totaal/lengte)
+        else:
+            # stuurt een lege lijst terug als 
+            l = []
+
+        return l
+
+#####################################################################################################################################
+
+class Lichtsensor(Sensor):
+    #bij max gaan ze naar beneden, bij min gaan ze omhoog
+    def __init__(self, max=80000, min=10000):
+        super().__init__(min, max)
+                
+#####################################################################################################################################
+
+class Temperatuur(Sensor):
+    # Bij max gaan ze naar beneden, bij min gaan ze omhoog
+    def __init__(self, max=22, min=18):
+        super().__init__(min, max)
+
+#####################################################################################################################################
+
+class Luchtvochtigheid(Sensor):
+    #bij max gaan ze naar beneden, bij min gaan ze omhoog
+    def __init__(self, max=80000, min=10000):
+        super().__init__(min, max)
+
+#####################################################################################################################################
 
 class Arduino():
 
     # arduino is 1 item uit de lijst arduino_port uit connections
-    def __init__(self, arduino, status="onbekend"):
+    def __init__(self, arduino, sensor=Sensor(), status="onbekend"):
         self.port = arduino[0]
         self.naam = arduino[1].split(" (COM")[0]
+        self.sensor = sensor
         self.serial = serial.Serial(self.port, data_transfer.CONST_BAUT)
         self.status = status
         time.sleep(5)
@@ -32,139 +172,3 @@ class Arduino():
         self.naam = naam
 
 #####################################################################################################################################
-
-class Lichtsensor(Arduino):
-    #bij max gaan ze naar beneden, bij min gaan ze omhoog
-    def __init__(self, arduino, status="onbekend", max=80000, min=10000):
-        super().__init__(arduino, status)
-        self.min = min
-        self.max = max
-        self.data = {}
-
-    # verander het bovenste limiet
-    def change_max(self, value):
-        if value != self.max and value > self.min:
-            data_transfer.change_limiet(self.port, 'max', value)
-            self.max = value
-    
-    # verander het onderste limiet
-    def change_min(self, value):
-        if value != self.min and value < self.max:
-            data_transfer.change_limiet(self.port, 'min', value)
-            self.min = value
-    
-    # verzamel de data van de arduino gebasseerd op datum
-    def collect_data(self):
-        datum = datetime.datetime.now()
-        jaar = datum.year
-        maand = datum.month
-        dag = datum.month
-        uur = datum.hour
-
-        data = data_transfer.get_data(self.port)
-
-        data_added = False
-
-        if jaar in self.data:
-            if maand in self.data[jaar]:
-                if dag in self.data[jaar][maand]:
-                    if uur in self.data[jaar][maand][dag]:
-                        self.data[jaar][maand][dag][uur].append(data)
-                        data_added = True
-
-        # als het geen nieuwe sleutel heeft gecreëerd en dus geen data heeft tegevoegd,
-        # wordt de data nu toegevoegd aan een al bestaande lijst
-        if not data_added:
-            self.data[jaar][maand][dag][uur] = [data]
-            data_added = True
-
-    def return_data(self, begindatum, einddatum):
-        pass
-
-
-#####################################################################################################################################
-
-class Temperatuur(Arduino):
-    # Bij max gaan ze naar beneden, bij min gaan ze omhoog
-    def __init__(self, arduino, status="onbekend", max=22, min=18):
-        super().__init__(arduino, status)
-        self.min = min
-        self.max = max
-        self.data = {}
-    
-    # verander het bovenste limiet
-    def change_max(self, value):
-        if value != self.max and value > self.min:
-            data_transfer.change_limiet(self.port, 'max', value)
-            self.max = value
-    
-    # verander het onderste limiet
-    def change_min(self, value):
-        if value != self.min and value < self.max:
-            data_transfer.change_limiet(self.port, 'min', value)
-            self.min = value
-
-    # verzamel de data van de arduino gebasseerd op datum
-    def collect_data(self):
-        datum = datetime.datetime.now()
-        jaar = datum.year
-        maand = datum.month
-        dag = datum.month
-
-        data = data_transfer.get_data(self.port)
-
-        if jaar in self.data:
-            if maand in self.data[jaar]:
-                if dag in self.data[jaar][maand]:
-                    self.data[jaar][maand][dag].append(data)
-                else:
-                    self.data[jaar][maand][dag] = [data]
-            else:
-                self.data[jaar][maand][dag] = [data]
-        else:
-            self.data[jaar][maand][dag] = [data]
-
-#####################################################################################################################################
-
-class Luchtvochtigheid(Arduino):
-    #bij max gaan ze naar beneden, bij min gaan ze omhoog
-    def __init__(self, arduino, status="onbekend", max=80000, min=10000):
-        super().__init__(arduino, status)
-        self.min = min
-        self.max = max
-        self.data = {}
-
-    # verander het bovenste limiet
-    def change_max(self, value):
-        if value != self.max and value > self.min:
-            data_transfer.change_limiet(self.port, 'max', value)
-            self.max = value
-    
-    # verander het onderste limiet
-    def change_min(self, value):
-        if value != self.min and value < self.max:
-            data_transfer.change_limiet(self.port, 'min', value)
-            self.min = value
-    
-    # verzamel de data van de arduino gebasseerd op datum
-    def collect_data(self):
-        datum = datetime.datetime.now()
-        jaar = datum.year
-        maand = datum.month
-        dag = datum.month
-
-        data = data_transfer.get_data(self.port)
-
-        if jaar in self.data:
-            if maand in self.data[jaar]:
-                if dag in self.data[jaar][maand]:
-                    self.data[jaar][maand][dag].append(data)
-                else:
-                    self.data[jaar][maand][dag] = [data]
-            else:
-                self.data[jaar][maand][dag] = [data]
-        else:
-            self.data[jaar][maand][dag] = [data]
-
-#####################################################################################################################################
-
