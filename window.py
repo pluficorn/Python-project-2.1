fb0300ca0da2d53f21f8cf6594e7d0cfeec679c8
Naomi Verkade <57986773+pluficorn@users.noreply.github.com>
2020-11-23 17:30:32 +0100

    update

--- window.py
+++ window.py
@@ -10,6 +10,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from arduino import *
 import connections
 import data_transfer
+import datetime
 
 class Window(Frame):
     
@@ -45,44 +46,31 @@ class Window(Frame):
         self.dropdown(arduinos, lichtintensiteit)
         self.dropdown(arduinos, rolluiken)
 
-        omhoogButton = Button(rolluiken, text="Omhoog", command = omhoog_command) #function placeholder
+        omhoogButton = Button(rolluiken, text="Omhoog", command = omhoog_command )
         omhoogButton.place(x=460, y=200)
-        omlaagButton = Button(rolluiken, text="Omlaag", command = omlaag_command) #function placeholder
+        omlaagButton = Button(rolluiken, text="Omlaag", command = omlaag_command)
         omlaagButton.place(x=460, y=250)
 
         #statusoverzicht page
-        tree['columns'] = ("Rolluik", "Status", "Temperatuur", "Lichtintensiteit")
+        tree['columns'] = ("Rolluik", "Status", "Laatste meting")
         tree.column("#0", width=0, minwidth=25)
         tree.column("Rolluik", anchor=W, width=120)
         tree.column("Status", anchor=W, width=60)
-        tree.column("Temperatuur", anchor=W, width=120)
-        tree.column("Lichtintensiteit", anchor=W, width=160)
+        tree.column("Laatste meting", anchor=W, width=120)
 
         tree.heading("#0", text="", anchor=W)
         tree.heading("Rolluik", text="Rolluik", anchor=W)
         tree.heading("Status", text="Status", anchor=W)
-        tree.heading("Temperatuur", text="Huidige temperatuur", anchor=W)
-        tree.heading("Lichtintensiteit", text="Huidige lichtintensiteit", anchor=W)
+        tree.heading("Laatste meting", text="Laatste meting", anchor=W)
 
-        tree.insert(parent='', index='end', iid=0, text="", values=("Woonkamer", "Open", 20, 42))
-        tree.insert(parent='', index='end', iid=1, text="", values=("Slaapkamer", "Dicht", 46, 12))
+        tree.insert(parent='', index='end', iid=0, text="", values=("Woonkamer", "Open", 20)) #placeholder values
+        tree.insert(parent='', index='end', iid=1, text="", values=("Slaapkamer", "Dicht", 46)) #placeholder values
         tree.pack(pady=20)
 
-        #page temperatuur en lichtintensiteit
+        #linechart
         self.lineChartTemperatuur(dataset, temperatuur)
-        dropdown = StringVar(temperatuur)
-        dropdown.set("Dag") #default value
-        self.lineChartLicht(dataset2, lichtintensiteit)
-        dropdown = StringVar(lichtintensiteit)
-        dropdown.set("Dag") #default value
-
-        dropdownMenu = OptionMenu(temperatuur, dropdown,"Dag", "Week", "Maand", "Jaar")
-        dropdownMenu.pack()
-        dropdownMenu.place(x=350, y=0)
-        dropdownMenu = OptionMenu(lichtintensiteit, dropdown,"Dag", "Week", "Maand", "Jaar")
-        dropdownMenu.pack()
-        dropdownMenu.place(x=350, y=0)
-        
+        self.lineChartLicht(dataset, lichtintensiteit)
+     
         #font
         myFont = TkFont.Font(family="helvetica", size=10)
         helpmijTekst.configure(font=myFont, state="disabled")
@@ -91,22 +79,22 @@ class Window(Frame):
         self.openHelpmij()
 
     def lineChartTemperatuur(self, dataset, locatie):
-        df = DataFrame(dataset,columns=['Dag','Gemiddelde_temp'])
+        df = DataFrame(dataset,columns=['uur','gemiddelde'])
         figure = plt.Figure(figsize=(4,3), dpi=100)
         ax = figure.add_subplot(111)
         line = FigureCanvasTkAgg(figure, locatie)
         line.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
-        df = df[['Dag','Gemiddelde_temp']].groupby('Dag').sum()
+        df = df[['uur','gemiddelde']].groupby('uur').sum()
         df.plot(kind='line', legend=True, ax=ax, color='b',marker='o', fontsize=10)
         ax.set_title('Gemiddelde temperatuur')   
 
     def lineChartLicht(self, dataset, locatie):
-        df = DataFrame(dataset,columns=['Dag','Gemiddelde_licht'])
+        df = DataFrame(dataset,columns=['uur','gemiddelde'])
         figure = plt.Figure(figsize=(4,3), dpi=100)
         ax = figure.add_subplot(111)
         line = FigureCanvasTkAgg(figure, locatie)
         line.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
-        df = df[['Dag','Gemiddelde_licht']].groupby('Dag').sum()
+        df = df[['uur','gemiddelde']].groupby('uur').sum()
         df.plot(kind='line', legend=True, ax=ax, color='r',marker='o', fontsize=10)
         ax.set_title('Gemiddelde lichtintensiteit')  
         
@@ -141,20 +129,20 @@ class Window(Frame):
         helpmij.pack(fill="both", expand=1)
 
     def dropdown(self, lijst, locatie):
-        dropdown.set(lijst[0]) # default value, eerste index
+        dropdown.set(lijst[0])
         widget = OptionMenu(locatie, dropdown, *lijst)
         widget.pack()
 
-    def doNothing(self):
-        pass
+    # def doNothing(self):
+    #     pass
 
     def table(self):
-        tree= Treeview(statusoverzicht)
+        tree=Treeview(statusoverzicht)
 
 #create a main window
 root=tk.Tk() 
 root.title("Centrale")
-root.configure(background='red')
+root.configure()
 root.geometry('600x500')
 
 #variabele
@@ -175,31 +163,21 @@ meerinfo = "\nVoor meer informatie kunt u contact opnemen met 06-12345678 of mai
 helpmijtekst = ("Welkom! \n\n" + "In deze centrale kunt u alles inzien omtrent de rolluiken, hieronder volgt een korte uitleg van alle \nonderdelen binnen deze centrale: \n" 
                 + exittekst + helptekst + rolluikenuitleg + statusoverzichtuitleg + temperatuuruitleg + lichtuitleg + meerinfo)
 
-
-# # haal de arduino in de porten op van Connections
-# arduino_port = connections.arduino_port
-# # lege list om de arduinos in op te slaan
-# arduinos = []
-# # voor elke arduino...
-# for ar in arduino_port:
-#     # Kijk welke sensor wordt meegegeven
-#     sensor = data_transfer.get_sensor(ar[0])
-#     # Voeg een arduino klasse Arduino toe aan de list
-#     arduinos.append(Arduino(ar, sensor))
+#Lijst arduinos
 arduinos = connections.arduinos
-# arduinos.append(Arduino(('COM4', 'Arduino Uno (COM4)', 'USB VID:PID=2341:0043 SER=75834303638351E03130 LOCATION=1-2'), Sensor()))
 
 # geef wel nog de geselecteerde arduino voor het command. Dus {arduino}.status...
 omlaag_command = arduinos[0].status_omlaag()
 omhoog_command = arduinos[0].status_omhoog()
 
-#placeholders
-dataset = {'Dag': [1920,1930,1940,1950,1960,1970,1980,1990,2000,2010], #placeholder voor linechart temperatuur
-         'Gemiddelde_temp': [9.8,12,8,7.2,6.9,7,6.5,6.2,5.5,6.3]}  
-dataset2 = {'Dag': [1920,1930,1940,1950,1960,1970,1980,1990,2000,2010], #placeholder voor linechart lichtintensiteit
-         'Gemiddelde_licht': [9.8,12,8,7.2,6.9,7,6.5,6.2,5.5,6.3]}  
-dropdown = StringVar(rolluiken)
+#variabele
+dataset = Sensor.return_data(datetime.now, datetime.now)
+# dataset = {'Dag': [1920,1930,1940,1950,1960,1970,1980,1990,2000,2010], #placeholder voor linechart temperatuur
+#          'Gemiddelde_temp': [9.8,12,8,7.2,6.9,7,6.5,6.2,5.5,6.3]}  
+# dataset2 = {'Dag': [1920,1930,1940,1950,1960,1970,1980,1990,2000,2010], #placeholder voor linechart lichtintensiteit
+#          'Gemiddelde_licht': [9.8,12,8,7.2,6.9,7,6.5,6.2,5.5,6.3]}  
+#tijdelijke  = ["arduino2", "arduino1"]
 
 #class aanroepen/instellen + start window
 GUI=Window(root)
-root.mainloop()
+root.mainloop()     
